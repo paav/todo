226b465391b2d8878b5fadb216e2cf53ef7d26e9
Alexey Panteleiev <paav@inbox.ru>
2015-05-22 09:54:37 +0700

    Remove obsolete code chunks

--- lib/todo.py
+++ lib/todo.py
@@ -34,9 +34,6 @@ class Model(object):
             if k in self._attrs:
                 self._attrs[k] = attrs[k]
 
-    @property
-    def DBFILE(cls):
-        return cls._DBFILE
 
 class Task(Model):
     def __init__(self, attrs={}, isnew=True, tags=[]):
@@ -122,41 +119,8 @@ class Task(Model):
         """Converts sqlite3.Row to attrs dict."""
         return dict(zip(row.keys(), list(row)))
 
-    def _findAll(self, cond='', params=()):
-        cur = self.dbconn.cursor()
-        sql = 'SELECT * FROM task WHERE done_date IS NULL'
-        if cond:
-            sql += ' AND %s ' % cond
-        sql +=  ' ORDER BY priority DESC'
-        cur.execute(sql, params)
-        tasks = []
-        for row in cur.fetchall():
-            attrs = dict(zip(row.keys(), list(row)))
-            tasks.append(Task(attrs, isnew=False))
-        return tasks
-
-    def findById(self, id):
-        cur = self.dbconn.cursor()
-        sql = 'SELECT * FROM task WHERE id=?'
-        cur.execute(sql, (id,))
-        task = Task(Task.row2attrs(cur.fetchone()), isnew=False)
-        return task
-
     def gettagnames(self):
         return [ tag.name for tag in self._tags ]
-
-    def tag_names(self):
-        sql = 'SELECT * FROM tag WHERE task_id=?'
-        self.dbcur.execute(sql, (self.getAttr('id'),))
-        tag_names = []
-        for row in self.dbcur.fetchall():
-            tag_names.append(row['name'])
-        return tag_names
-
-    @staticmethod
-    def row2attrs(row):
-        """Convert sqlite3.Row to attrs dict."""
-        return dict(zip(row.keys(), list(row)))
 
     def save(self):
         cur = self._dbcon.cursor()
@@ -184,9 +148,6 @@ class Task(Model):
             Tag().delall('task_id=?', (pk,))
             Tag().saveall(self.tags)
 
-    def delete(self):
-        self.deleteById(self.getAttr('id'))
-
     @property
     def priority(self):
         return self._attrs['priority']
@@ -207,53 +168,14 @@ class Task(Model):
         self._dbcon.commit()
         Tag().delall('task_id=?', (id,))
 
-    def getAttr(self, name):
-        return self._attrs[name]
-
-    def setAttr(self, name, value):
-        self._attrs[name] = value
-
-    def edit(self):
-        lines = []
-        lines.append(self.attrs['title'])
-        if self.getAttr('body'):
-            lines.extend(self.attrs['body'].split('\n'))
-        self.vim.open_edit_win()
-        self.vim.write(lines)
-
-    def finish(self):
-        sql = 'UPDATE task SET done_date=? WHERE id=?'
-        self.dbcur.execute(sql, (time.time(), self.getAttr('id')))
-        self.dbconn.commit()
-
 
 class TaskList(object):
     def __init__(self):
-        self.cond = ''
-        self.params = ()
-        self.last_task_at_cursor = None
-        self.tbody_1strow_lnum = 1
-        self.filter_by_tags = []
         self._tasks = []
         self._filter = {}
-        # self.populate() 
 
     def load(self):
         self._tasks = Task().findAll()
-
-    # TODO: change name
-    def gettasks(self):
-        self._tasks = Task().findAll()
-        tasks = []
-        for task in self._tasks: 
-            attrs = self._convnone(task.attrs)
-            tags = []
-            for tag in task.tags: 
-                tags.append(self._convnone(tag.attrs))
-            attrs['tags'] = tags
-            attrs['isnew'] = task.isnew
-            tasks.append(attrs)
-        return tasks
 
     @property
     def filter(self):
@@ -278,84 +200,6 @@ class TaskList(object):
     def _createvimlist(tasks):
         return [ task.todict() for task in tasks ]
 
-    @staticmethod
-    def _convnone(attrs):
-        for k, v in attrs.iteritems():
-            if v is None: 
-                attrs[k] = ''
-        return attrs
-
-    def render(self):
-        self.vim.open_main_win()
-        self.vim.write(self.create_table(), offset=2)
-        if self.last_task_at_cursor:
-            lnum = self.last_task_lnum()
-        else:
-            lnum = self.tbody_1strow_lnum 
-        self.vim.set_cursor(lnum)
-
-    def apply_tag_filter(self, tags):
-        if tags:
-            cond = ' and '.join(['name=?'] * len(tags))
-            tags = Tag.inst().findAll(cond, tuple(tags)) 
-            task_ids = map(lambda t: t.task_id, tags)
-            self.cond = 'id IN(' + ','.join(['?'] * len(task_ids)) + ')'
-            self.params = tuple(task_ids)
-        else:
-            self.cond = ''
-            self.params = ()
-        self.render()
-
-    def create_table(self):
-        self.populate()
-        labels = Task.inst().attr_labels()
-        labels['tag'] = 'Tag'
-        format = '%(create_date)-10s%(title)-37s%(tag)-12s%(priority)-s'
-        # Add ~ for table head highlighting
-        thead = '~' + format % labels 
-        tdelim = '─' * self.vim.win_width() 
-        tbody_lines = []
-        # TODO: do something with 5
-        self.tbody_1strow_lnum = 5
-        for task in self.tasks:
-            # TODO: drop copy() from here
-            fields = task.attrs.copy()
-            try:
-                fields['tag'] = task.tag_names()[0]
-            except IndexError:
-                fields['tag'] = ''
-            # Add ` for row cell highlighting
-            pri = fields['priority']
-            pri_pfx = ''
-            if pri > 5:
-                pri_pfx = '!1'
-            elif pri > 2:
-                pri_pfx = '!2'
-            tbody_lines.append('`' + pri_pfx + format % self.format_attrs(fields)) 
-        table_lines = [thead, tdelim] + tbody_lines
-        return table_lines
-
-    @classmethod
-    def format_attrs(cls, attrs):
-        attrs['create_date'], = cls.format_dates(attrs['create_date'])
-        return attrs
-
-    @staticmethod
-    def format_dates(*tstamps):
-        def format_date(tstamp):
-            # %-d -- works only for unix like os
-            return unicode(datetime.fromtimestamp(tstamp).strftime('%-d %b'),
-                           'utf8') 
-        return map(format_date, tstamps)
-
-    def get_task_at_cursor(self):
-        lnum = self.vim.get_line_number()
-        if lnum < self.tbody_1strow_lnum:
-            return None
-        task = self.tasks[lnum - self.tbody_1strow_lnum]
-        self.last_task_at_cursor = task
-        return task 
-
     def findbyid(self, id):
         return [task for task in self._tasks if task.id == id][0]
 
@@ -372,26 +216,6 @@ class TaskList(object):
 
     def add(self, task):
         self._tasks.append(task)
-
-    def last_task_lnum(self):
-        last_task = self.last_task_at_cursor
-        try:
-            return self.get_index_of_task(last_task) + self.tbody_1strow_lnum
-        except ValueError:
-            return self.tbody_1strow_lnum
-
-    def get_index_of_task(self, task):
-        id = task.getAttr('id')
-        for i, t in enumerate(self.tasks):
-            if t.getAttr('id') == id:
-                return i
-        raise ValueError
-
-    def get_cursor(self):
-        return self.vim.get_cursor()
-
-    def set_cursor(self, lnum):
-        return self.vim.set_cursor(lnum)
 
 
 class Tag(Model):
@@ -425,16 +249,6 @@ class Tag(Model):
         return [Tag({'name': attrs['name'], 'task_id': attrs['task_id']})
                 for attrs in attrslist]
 
-    def save(self):
-        # TODO: same as in Task.save()
-        attrs = self._attrs
-        cols = ','.join(attrs.keys())
-        values = ('?,' * len(attrs))[:-1]
-        params = tuple(attrs.values()) 
-        sql = 'INSERT INTO tag (' + cols + ') VALUES(' + values + ')'
-        self._dbcon.cursor().execute(sql, params)
-        self._dbcon.commit()
-        
     def saveall(self, tags):
         cur = self._dbcon.cursor()
         sql = 'INSERT INTO tag (name,task_id) VALUES'
